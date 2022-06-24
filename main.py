4fb23ac51271418bd88b667524ee2a1c79c2dc78
x8g <98675255+x8g@users.noreply.github.com>
2022-06-23 21:04:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 import threading
+import time
 import requests
 from pystyle import Colorate, Colors
-import os; os.system("cls||clear"); os.system(f"title github/x8g"); os.system("mode con:cols=60 lines=26")
+import os
 
-url = 'https://camo.githubusercontent.com/d07c880342f0935b11b282178f79c6808f97c662cc9d193859ec79cee9af26bf/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d62366d'
+os.system(f'title github/x8g')
+
+# improved every bit of this code and made it "easier" to use!
+
+print('User: ')
+user = input()
+print('')
+
+print('URL: ')
+url = input()
+print('')
+
+print('Thread Amount?: ')
+thread = input()
+print('')
 
 
-def do_request():
+def view():
  while True:
    response = requests.get(url)
    if response.status_code == 200:
-    print(Colorate.Horizontal(Colors.red_to_purple, "viewed profile!"))
-
+    print(Colorate.Horizontal(Colors.red_to_purple, f'viewed profile! | user: ' + user))
+    
+    
 threads = []
 
-for i in range(500):
-    t = threading.Thread(target=do_request)
+for i in range(int(thread)):
+    t = threading.Thread(target=view)
     t.daemon = True
     threads.append(t)
 
-for i in range(500):
+for i in range(int(thread)):
     threads[i].start()
 
-for i in range(500):
+for i in range(int(thread)):
     threads[i].join()
