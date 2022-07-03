af24e176d6e2d91cb39b8f5b57e79fe3eb6a343c
x8g <98675255+x8g@users.noreply.github.com>
2022-07-03 09:45:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,58 @@
-import threading
-import time
-import requests
-from pystyle import Colorate, Colors
-import os
+try:
+    import requests, os, sys, threading
+    from pystyle import *
+except Exception as e:
+    print(f'Missing Imports, Or Error: [{e}]')
 
-os.system(f'title github/x8g')
+# don't skid!
 
-# improved every bit of this code and made it "easier" to use!
+banner = """
+                                .:                           :.         thanks                
+                              !?.            :   ~            ^J^             for          
+                            ?B!             G5   &&7            YG^              the       
+                          ?&G.      :^    .#@5   &@@&?  .^.      ^&#~               banner    
+                        J@@!      ~5~    :&@@5   &@@@Y    ?Y.      5@#~  xtekky               
+                        5@@7    !#G..Y~ ~@@@@5   &@@?      ~#G:    P@@!        I         
+                         ^&@B:7&@!.Y@@@B@@@@@5   &@@!        P@B^~&@G.          love        
+                           5@@@B^J@@@@@@@&&@@5   &@@!         ~&@@@!                you!    
+                           5@@@5^@@@5.G@&:G@@5   &@@!       ...&@@@!      from              
+                         ~&@B:5@P:#@&  !. G@@5   &@@!      ??^&@!~&@B.        zt#7380          
+                        P@@7   ^&P.G@?    G@@5   &@@!    J&?:&B.   P@@7              aka   
+                        J&@!     PP J&    G@@5   &@@!  ?@@^.B7     5@#~                 github.com/x8g
+                          ?&G.    ~? !!   B@@5   &@@?7&@&..Y.    ~&#^                   
+                            7B!    .^     J@@5   &@@@@@G  ^    .YG^                     
+                              !?.          &@5   &@@@@J       :?^                       
+                                :.         ~@5   &@@@~       ..                         
+                                            BP   &@&:                                   
+                                            ^Y   &B.                                    
+                                             :   7                                      
+"""
+os.system(f'title GVB ^| Status: ^| loading..')
+Anime.Fade(Center.Center(banner), Colors.blue_to_red, Colorate.Vertical, interval=0.01, time=6)
+Anime.Fade(Center.Center("Github View Botter"), Colors.blue_to_red, Colorate.Vertical, interval=0.01, time=2)
+Anime.Fade(Center.Center("By zt#7380"), Colors.blue_to_red, Colorate.Vertical, interval=0.01, time=3)
+Anime.Fade(Center.Center("uhh bot views on github?"), Colors.blue_to_red, Colorate.Vertical, interval=0.01, time=5)
 
-print('User: ')
-user = input()
-print('')
+os.system(f'title GVB ^| Status: ^| loaded!')
+user = input(Colorate.Horizontal(Colors.blue_to_purple, 'User?: '))
 
-print('URL: ')
-url = input()
-print('')
+url = input(Colorate.Horizontal(Colors.blue_to_purple, 'Url?: '))
 
-print('Thread Amount?: ')
-thread = input()
-print('')
+threads = input(Colorate.Horizontal(Colors.blue_to_purple, 'Threads?: '))
 
-
+os.system(f'title Github Views ^| github/x8g')
 def view():
  while True:
    response = requests.get(url)
    if response.status_code == 200:
-    print(Colorate.Horizontal(Colors.red_to_purple, f'viewed profile! | user: ' + user))
-    
-    
-threads = []
+    print(Colorate.Horizontal(Colors.blue_to_purple, f'viewed profile! | user: ' + user))
+   else:
+    print(Colorate.Horizontal(Colors.blue_to_purple, 'Error viewing ' + user + ' profile'))
+    sys.exit()
 
-for i in range(int(thread)):
-    t = threading.Thread(target=view)
-    t.daemon = True
-    threads.append(t)
+# I know its still slow even with the threads don't bully me :(    
 
-for i in range(int(thread)):
-    threads[i].start()
+while True:
+        if threading.active_count() < int(threads):
+            threading.Thread(target=view).start()
 
-for i in range(int(thread)):
-    threads[i].join()
