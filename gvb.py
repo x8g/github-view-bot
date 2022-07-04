c1c877660890a97660a26b7c1f251655241b65e5
zt#7380 <98675255+x8g@users.noreply.github.com>
2022-07-04 13:37:04 -0400

    Update gvb.py
    added headers to try and make it faster!

--- gvb.py
+++ gvb.py
@@ -5,6 +5,20 @@ except Exception as e:
     print(f'Missing Imports, Or Error: [{e}]')
 
 # don't skid!
+headers = {
+"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+"accept-encoding": "gzip, deflate, br",
+"accept-language": "en-US,en;q=0.9",
+"cache-control": "max-age=0",
+"sec-ch-ua-mobile": "?0",
+"sec-ch-ua-platform": "Windows",
+"sec-fetch-dest": "document",
+"sec-fetch-mode": "navigate",
+"sec-fetch-site": "cross-site",
+"sec-fetch-user": "?1",
+"upgrade-insecure-requests": "1",
+"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.66 Safari/537.36 Edg/103.0.1264.44"
+}
 
 banner = """
                                 .:                           :.         thanks                
@@ -31,7 +45,7 @@ os.system(f'title GVB ^| Status: ^| loading..')
 Anime.Fade(Center.Center(banner), Colors.blue_to_red, Colorate.Vertical, interval=0.01, time=6)
 Anime.Fade(Center.Center("Github View Botter"), Colors.blue_to_red, Colorate.Vertical, interval=0.01, time=2)
 Anime.Fade(Center.Center("By zt#7380"), Colors.blue_to_red, Colorate.Vertical, interval=0.01, time=3)
-Anime.Fade(Center.Center("most overkill github view botter you've prolly seen LOL."), Colors.blue_to_red, Colorate.Vertical, interval=0.01, time=5)
+Anime.Fade(Center.Center("uhh bot views on github?"), Colors.blue_to_red, Colorate.Vertical, interval=0.01, time=5)
 
 os.system(f'title GVB ^| Status: ^| loaded!')
 user = input(Colorate.Horizontal(Colors.blue_to_purple, 'User?: '))
@@ -43,7 +57,7 @@ threads = input(Colorate.Horizontal(Colors.blue_to_purple, 'Threads?: '))
 os.system(f'title GVB ^| Botting ^| ' + user + ' ^| github/x8g')
 def view():
  while True:
-   response = requests.get(url)
+   response = requests.get(url, headers=headers)
    if response.status_code == 200:
     print(Colorate.Horizontal(Colors.blue_to_purple, f'viewed profile! | user: ' + user))
    else:
