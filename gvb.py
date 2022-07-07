24d8b9f00310c40fea52ff245e184dc3587f51ab
zt#7380 <98675255+x8g@users.noreply.github.com>
2022-07-07 18:00:30 -0400

    Update gvb.py
    removed useless headers.

--- gvb.py
+++ gvb.py
@@ -7,16 +7,6 @@ except Exception as e:
 # don't skid!
 headers = {
 "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-"accept-encoding": "gzip, deflate, br",
-"accept-language": "en-US,en;q=0.9",
-"cache-control": "max-age=0",
-"sec-ch-ua-mobile": "?0",
-"sec-ch-ua-platform": "Windows",
-"sec-fetch-dest": "document",
-"sec-fetch-mode": "navigate",
-"sec-fetch-site": "cross-site",
-"sec-fetch-user": "?1",
-"upgrade-insecure-requests": "1",
 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.66 Safari/537.36 Edg/103.0.1264.44"
 }
 
